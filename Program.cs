5dec6913a908120ead70994dea26c97c8a42fb33
Emre Basarir <yunu_emre_kardes@outlook.com>
2022-09-25 19:48:19 +0300

    Methods commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // erişim_belirteci geri_donuctipi metot_adi(parametreListesi/argüman)
+            //{
+                //komutlar;
+                //return;  (Geri dönmek için)
+            //}
+
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a,b); 
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek =new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttırVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a+b));        
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return(deger1 + deger2); 
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttırVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return(deger1 + deger2);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+(C# Methods)[https://app.patika.dev/courses/csharp-101/1-metot-nedir]
